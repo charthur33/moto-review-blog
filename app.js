c9945759d93c707d44cec92f79bf1f4fe90baba0
Chris <c.william.arthur@gmail.com>
2022-02-11 12:54:27 -0800

    add contact pages and connect nodemailer

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const nodemailer = require("nodemailer");
+const { propertyOf } = require("lodash");
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -13,7 +16,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let allPosts = [];
@@ -36,10 +39,10 @@ const Post = mongoose.model("Post", postsSchema);
 
 
 //***********ALL ROUTES*************** */
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   //Find posts in the DB and render them on homepage
 
-  Post.find({}, function(err, foundPosts){
+  Post.find({}, function (err, foundPosts) {
     if (err) {
       console.log(err);
     } else {
@@ -51,46 +54,56 @@ app.get("/", function(req, res) {
   });
 });
 
-app.get("/about", function(req, res) {
-  res.render('about', {aboutContent: aboutContent});
+app.get("/about", function (req, res) {
+  res.render('about', { aboutContent: aboutContent });
 });
 
-app.get("/subscribe", function(req, res) {
-    res.render('subscribe');
+app.get("/subscribe", function (req, res) {
+  res.render('subscribe');
 });
 
-app.get("/contact", function(req, res) {
-  res.render('contact', {contactContent: contactContent});
+app.get("/contact", function (req, res) {
+  res.render('contact', { contactContent: contactContent });
 });
 
-app.get("/compose", function(req, res) {
-    res.render('compose');
+app.get("/compose", function (req, res) {
+  res.render('compose');
 });
 
-app.get("/success", function(req, res) {
-    res.render('success');
+app.get("/success", function (req, res) {
+  res.render('success');
 });
 
-app.get("/failure", function(req, res) {
-    res.render('failure');
+app.get("/failure", function (req, res) {
+  res.render('failure');
+});
+
+app.get("/msgSuccess", function (req, res) {
+  res.render('msgSuccess');
+});
+
+app.get("/msgFailure", function (req, res) {
+  res.render('msgFailure');
 });
 
 
-app.get("/posts/:postId", function(req, res) {
-    //PostID url comes from "read more" link (see home.ejs)
-    const postID = req.params.postId;
 
-    //Find and display correct post from DB per the post ID
-    Post.findById(postID, function (err, foundPost) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render('post', {
-          postTitle: foundPost.title,
-          postText: foundPost.content
-        });
-      }
-    });
+
+app.get("/posts/:postId", function (req, res) {
+  //PostID url comes from "read more" link (see home.ejs)
+  const postID = req.params.postId;
+
+  //Find and display correct post from DB per the post ID
+  Post.findById(postID, function (err, foundPost) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('post', {
+        postTitle: foundPost.title,
+        postText: foundPost.content
+      });
+    }
+  });
 
 });
 
@@ -99,13 +112,13 @@ app.post("/compose", function (req, res) {
   let post_title = req.body.postTitle;
   let post_text = req.body.postText;
   //Object that will store a complete blog post
-  const newPost = new Post ({
+  const newPost = new Post({
     title: post_title,
     content: post_text
   });
 
   //Callback prevents the page from reloading before new post is saved to the DB
-  newPost.save(function(err) {
+  newPost.save(function (err) {
     if (err) {
       console.log(err);
     } else {
@@ -119,10 +132,58 @@ app.post("/failure", function (req, res) {
   res.redirect("/subscribe");
 });
 
+app.post("/msgFailure", function (req, res) {
+  res.redirect("/contact");
+});
+
+//***********************CONTACT ************** */
+
+async function sendMail(name, email, message, subject, res) {
+  let msgSent = false;
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // use TLS
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS,
+    },
+    tls: {
+      // do not fail on invalid certs
+      rejectUnauthorized: false,
+    },
+  });
+
+  try {
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: process.env.GMAIL_USER,
+      to: process.env.GMAIL_USER,
+      subject: subject,
+      html: "<h3> From: " + name + "</h3><h3> Email: " + email + "</h3><p>" + message + "</p> "
+    });
+    res.redirect("/msgSuccess");
+  } catch (err) {
+    console.log(err);
+    res.redirect("/msgFailure");
+  }
+}
+
+
+app.post("/contact", function (req, res) {
+  let name = req.body.name;
+  let email = req.body.email;
+  let message = req.body.message;
+  let subject = req.body.subject;
+  sendMail(name, email, message, subject, res);
+});
+
+
 //***********************SUBSCRIBE/MAIL CHIMP ********/
 
 
-app.post("/subscribe", function(req, res) {
+app.post("/subscribe", function (req, res) {
   const firstName = req.body.fName;
   const lastName = req.body.lName;
   const email = req.body.email;
@@ -146,8 +207,8 @@ app.post("/subscribe", function(req, res) {
     auth: process.env.MC_API_KEY
   }
 
-  const request = https.request(url, options, function(response) {
-    response.on("data", function(data) {
+  const request = https.request(url, options, function (response) {
+    response.on("data", function (data) {
       if (response.statusCode == 200) {
         res.redirect("/success");
       } else {
